e88ac5f389b06d210e6dd9c331c40f5e3d32054c
treknuts <trevor.a.knutson@ndsu.edu>
2020-11-09 11:58:50 -0600

    Added functionality to grab links from a page and add them to the links queue

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-let axios = require('axios');
-let cheerio = require('cheerio');
-let fs = require('fs');
+let axios = require("axios");
+let cheerio = require("cheerio");
 
-const doStuff = () => {
-    axios.get('https://www.stackoverflow.com').then((response) => {
-        if (response.status == 200) {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            console.log(html);
-        }
-    }, (error) => console.log(error));
+const TEST_URL = "https://www.crawler-test.com/";
+let visited = {};
+let rootNode = {};
+let currentNode = {};
+let queue = [];
+let printList = [];
+let prevDepth = 0;
+let maxDepth = 5;
+
+class CreateLink {
+  constructor(url, depth, parent) {
+    this.url = url;
+    this.depth = depth;
+    this.parent = parent;
+    this.children = [];
+  }
 }
 
-doStuff();
+// const crawl = async (maxDepth) => {
+//   axios.get(TEST_URL).then(
+//     (response) => {
+//       if (response.status == 200) {
+//         const html = response.data;
+//         const $ = cheerio.load(html);
+//         console.log(html);
+//       }
+//     },
+//     (error) => console.log(error)
+//   );
+// };
+
+const findLinks = async () => {
+  axios.get(TEST_URL).then((response) => {
+    if (response.status == 200) {
+      let html = response.data;
+      const $ = cheerio.load(html);
+      $("a").each((value) => {
+        var link = $(value).attr("href").;
+        queue.push(link);
+      });
+      queue.forEach((url) => {
+        console.log(url);
+      });
+    }
+  });
+};
+
+findLinks();
